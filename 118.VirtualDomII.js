ab0cd76d28fff29dfa9589b4309d90569279da3e
CaseMisty <burningalive.fff@live.com>
2024-06-30 14:01:50 +0800

    Create 118.VirtualDomII.js

---
+++ 118.VirtualDomII.js
@@ -0,0 +1,89 @@
+/* Transcript of the coding question 118. Virtual DOM II - createElement https://bigfrontend.dev/problem/virtual-dom-II-createElement */
+
+// I'm sharing my screen now. Is it visible to everyone?  or Can everyone see it?
+// Create a function that constructs a custom Node called MyElement, which is a simplified version of a React element node.
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    // Not doing too much in this function, just accepting type, props, children, and other props, and returning them as an object.
+    props: {
+      props,
+      // It is worth noting that, by convention, the 'children' property is located within the 'props' object.
+      children: [...children],
+    },
+  };
+};
+
+// Following that, rename createElement as h.
+const h = createElement;
+
+// Let me execute it to see if the generated virtual DOM matches the expected result. Okay, the result is as intended
+const node = h(
+  "div",
+  {},
+  h("h1", {}, "This is "),
+  h(
+    "p",
+    { className: "paragraph" },
+    "a ",
+    h("button", {}, "button "),
+    "from ",
+    h("a", { href: "https://bfe.dev" }, h("b", {}, "BFE"), ".dev")
+  )
+);
+
+/**
+ * @param { MyElement } myElement - The element to render.
+ * @returns { HTMLElement }
+ */
+
+// Now it's time to implement the second function, render, which takes in a MyElement and returns the corresponding DOM element.
+const render = (myElement) => {
+  //When the parameter of render is a string type, we just return it directly.
+  if (typeof myElement === "string") {
+    return myElement;
+  }
+
+  // Deconstruct myElement and assign it to type and props. Extract the props and children as variables.
+  const {
+    type,
+    props: { children, ...props },
+  } = myElement;
+
+  // The type here represents the HTML tag name, so let's create it using the document.createElement method.
+  const element = document.createElement(type);
+  // and this will be a recursive  method, to prevent omissions /əˈmɪʃ(ə)n/, return the element here, this serves as the base case for terminating the recursion.
+
+  // In addition, we have to iterate through the props object with Object.entries and set each property to the element.
+  for (const [name, value] of Object.entries(props)) {
+    // We need to handle the className attribute independently because it's not called that in DOM elements.
+    if (name === "className") {
+      element.setAttribute("class", value);
+    } else {
+      // Set other attributes to the element.
+      element.setAttribute(name, value);
+    }
+  }
+  // All asttributes of element have been set. As we proceed, we should handle children elements.
+  // Here's the trick: the DOM.append method appends the passed element to the end of the element.
+  // It accepts an indefinite number of arguments, which can be DOM nodes or strings. When a string is passed,
+  // the append method will automatically wrap it in a TextNode, without requiring us to handle it manually.
+  // Use the map function to recursively call the render method and then destructure the return value.
+  // There's a catch : children can be not only elements but also strings. The current render method here is not sufficient to handle strings.
+  // So, we can tweak our render function to adapt to this. we will add this at the beginning of the function.
+  element.append(...children.map(render));
+
+  // By the time we reach this line, all the child elements have been appended to the element, and we simply return the element here.
+  return element;
+};
+
+// Finally, add some test code to check the call results and see if they aligns with the expected outcome. Okay, No issues detected. Everything works perfectly.
+
+const el = render(node);
+console.log(el);
+// That's it all from me, thanks for watching.  Over to you, Michael.
+// that's it from my side.
+
+// Oh no, it can't run anymore. Let's take a look at why. Let's add a breakpoint here to see why children is undefined.
+// Damn, I missed an 's' in the property name during destructuring. Such a rookie/silly/basic mistake.
+// a very rookie mistake
