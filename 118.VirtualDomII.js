1e82a80c727ca306a0c058bbbac661a7f17c95f9
CaseMisty <burningalive.fff@live.com>
2024-06-30 19:58:14 +0800

    Update 118.VirtualDomII.js

--- 118.VirtualDomII.js
+++ 118.VirtualDomII.js
@@ -2,8 +2,8 @@
 
 // I'm sharing my screen now. Is it visible to everyone?  or Can everyone see it?
 // Regarding this problem, we basically need to complete two functions.
-// First, we have `createElement`, which generates our custom `node` element.
-// Then, we have `render`, which takes our custom `node` and returns a `DOM` node. okay, lets do it.
+// First, the `createElement`, which generates our custom `node` element.
+// Then, the `render`, which takes our custom `node` and returns a `DOM` node. okay, lets do it.
 // Create a function that constructs a custom Node called MyElement, which is a simplified version of a React element node.
 const createElement = (type, props, ...children) => {
   return {
