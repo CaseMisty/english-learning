177b8a55c9cd3e1be9a210be2d8042c1f05345ab
CaseMisty <burningalive.fff@live.com>
2024-07-25 21:53:51 +0800

    Create 142. lit-html 1 - tagged templates.js

---
+++ 142. lit-html 1 - tagged templates.js
@@ -0,0 +1,15 @@
+function html(strs, ...args) {
+  
+  let res = '';
+  strs.forEach((item, i) => {
+    res+=item;
+    if (args[i]) res+=args[i];
+  })
+  return res;
+}
+
+
+// render the result from html() into the container
+function render(result, container) {
+  container.innerHTML = result
+}
