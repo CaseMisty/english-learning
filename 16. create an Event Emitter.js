945fc6e294b66a04e36de1b8ae99f161924db183
CaseMisty <burningalive.fff@live.com>
2024-07-28 22:04:10 +0800

    Update 16. create an Event Emitter.js

--- 16. create an Event Emitter.js
+++ 16. create an Event Emitter.js
@@ -1,42 +1,29 @@
-// today I'm going to get down to this question.
-// This class allows multiple subscribe calls for the same eventName, and it can return a release method.
+// please complete the implementation
 class EventEmitter {
-
-  // For this task, the class only needs one attribute: eventMap, it maintains a map that subscribes to the object.
-  //map:  (eventName,[callback...]),
-  // The key is eventName, and the value is an array composed of callback functions with the same eventName 
-  //key: eventName,value: An array of callback functions with the same eventName
-  eventMap = new Map();
+  events = {}
 
   subscribe(eventName, callback) {
-    if (!this.eventMap.has(eventName)) {
-      // And the requirement said that same callback could subscribe for same event multiple times, hence the type of values of this map is array.
-      // eventName and callback pair is added if it doesn't exist
-      this.eventMap.set(eventName, []);
-    }
-    //eventName exists, add callback to the corresponding array
-    this.eventMap.get(eventName).push(callback);
+    // And the requirement said that same callback could subscribe for same event multiple times, hence the type of values of this map is array.
+    // eventName and callback pair is added if it doesn't exist
+    this.events[eventName] ??= new Map();
+
+    // use a symbol to identify the callback we add
+    const key = Symbol();
+    // eventName exists, add callback to the corresponding map
+    this.events[eventName].set(key, callback);
 
     return {
-      //return an object which containing the release method
+      // return an object which containing the release method
       release: () => {
-        //find the corresponding callback and delete it
-        this.eventMap.get(eventName).forEach((item, index) => {
-          if (item == callback) {
-            this.eventMap.get(eventName).splice(index, 1);
-          }
-        })
+        this.events[eventName].delete(key);
       }
     }
   }
-  //  is used to trigger the callbacks, with args relayed
+  // is used to trigger the callbacks, with args relayed
   emit(eventName, ...args) {
-    if (this.eventMap.has(eventName)) {
-      //if eventName exists,call the callback in eventMap accordingly
-      this.eventMap.get(eventName).forEach(call => {
-        call.apply(this, args);
-      })
-    }
+    Array.from(this.events[eventName].values()).forEach(fn => {
+      fn.apply(this, args)
+    })
   }
 }
-// I seem to have forgotten to input the method name just now.
+// I seem to have forgotten to input the method name just now. Let me see what the error is
